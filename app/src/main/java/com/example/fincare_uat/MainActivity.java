6ebf3f01f9e2f523f808076f6cdd50b315530d06
mahendhar <mahendharjakkula96@gmail.com>
2019-11-15 00:53:38 +0530

    2nd commit with layout designs

--- app/src/main/java/com/example/fincare_uat/MainActivity.java
+++ app/src/main/java/com/example/fincare_uat/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.fincare_uat;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -14,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerview;
     ImageView locate;
+    Dialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dialog = new Dialog(this);
         recyclerview = (RecyclerView) findViewById(R.id.recyclerview);
         locate = (ImageView) findViewById(R.id.locate);
 
@@ -28,9 +35,23 @@ public class MainActivity extends AppCompatActivity {
         locate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                showPopup();
             }
         });
+    }
+
+    private void showPopup() {
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams wlp = window.getAttributes();
+        wlp.gravity = Gravity.TOP;
+        wlp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        wlp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        window.setAttributes(wlp);
+        dialog.setContentView(R.layout.locate_popup);
+        dialog.setCancelable(true);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.show();
+
     }
 
 
